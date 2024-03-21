1e91a187fdc82a6750c989fc29b31f8a3f740907
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2024-03-21 16:55:45 -0500

    made naive dft, working on linspace for tests

--- src/HaskellFFT.hs
+++ src/HaskellFFT.hs
@@ -1,7 +1,23 @@
 module HaskellFFT (dftNaive, fft) where
 
-dftNaive :: [Float] -> [Float]
-dftNaive a = a
+import Data.Complex
+
+dftNaiveSum :: RealFloat a => Int -> Int -> Int -> [Complex a] -> Complex a
+dftNaiveSum k n len [] = 0 :+ 0
+dftNaiveSum kInt nInt lenInt (x:xs) = do
+    let k = fromIntegral kInt
+    let n = fromIntegral nInt
+    let len = fromIntegral lenInt
+    let v1 = x * (exp (0 :+ (2 * pi * k * n / len)))
+    let v2 = (dftNaiveSum kInt (nInt + 1) lenInt xs)
+    v1 + v2
+
+dftNaiveIter :: RealFloat a => Int -> [Complex a] -> [Complex a]
+dftNaiveIter 0 xs = []
+dftNaiveIter n xs = (dftNaiveSum ((length xs) - n) 0 (length xs) xs) : (dftNaiveIter (n - 1) xs)
+
+dftNaive :: RealFloat a => [Complex a] -> [Complex a]
+dftNaive xs = dftNaiveIter (length xs) xs
 
 fft :: Int -> Int
 fft a = a + 1
--- test/Main.hs
+++ test/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
+import Data.Complex
 import Test.HUnit
-
 import HaskellFFT
 
 allClose :: (Ord a, Num a) => [a] -> [a] -> a -> Bool
@@ -21,5 +21,19 @@ testAllClose3 :: Test
 testAllClose3 = TestCase $ do
     assertBool "Vals of different lengths should not be all close" $ not $ allClose [10, 3] [10] 0.1
 
+-- createLinspace :: RealFloat a => Int -> Int -> a -> a -> [a]
+-- createLinspace 0 _ _ _ = []
+-- createLinspace n tot a b = 
+
+-- createComplexSine :: Int -> Int -> [Complex a]
+-- createComplexSine 0 tot = []
+-- createComplexSine n tot = do
+--     let x = 2 * pi * (fromIntegral (tot - n)) / (fromIntegral (tot))
+--     let val = sin(x)
+--     let complex = ((val) :+ 0)
+--     complex : createComplexSineIter (n - 1) tot
+
 main :: IO Counts
-main = runTestTT $ TestList [testAllClose1, testAllClose2, testAllClose3]
+main = do
+    putStrLn $ show $ dftNaive [0 :+ 0, 1 :+ 0, 0 :+ 0]
+    runTestTT $ TestList [testAllClose1, testAllClose2, testAllClose3]
